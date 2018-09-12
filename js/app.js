8214948b6a7ebc670beb234eea090a76fc435254
hiwot nega <hiwotmanaye2@gmail.com>
2018-09-11 20:40:12 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+    
+  var response = prompt('Are you married?');
+  if(response.toLowerCase() === 'yes'|| response.toUpperCase() === 'YES'){
+ console.log('where enjoy your honeymoon?');
+  }
+   else {
+       console.log('then you are a single....!!!');
+   }
+
+var ageResponse = prompt('Are you 18 years old or older?');
+if(ageResponse.toLowerCase() === 'yes' || ageResponse.toUpperCase() === 'YES'){
+    console.log('you are eligible to work!!!');
+}
+else {
+    console.log('grow up kiddo!!!');
+}
+
+var something = prompt('did you see something in the sky?')
+    if(something.toUpperCase() === 'yes'|| something.toUpperCase() === 'YES'){
+        console.log('i am not imagining!!!');
+    }
+else {
+    console.log('i guess i am imagining things!!!')
+}
+
+
+var movies = prompt('Have you seen Beauty and the Beast?');
+if(movies.toLowerCase() === 'no' || movies.toUpperCase()==='NO' ){
+    console.log('ohh u miss it!!')
+}
+else if(movies.toLowerCase() === 'yes' ||movies.toUpperCase()==='YES'){
+    console.log('ohh Me too what a nice movie !!')
+}
+else {
+    console.log('answer yes or no please!!!');
+}
+
+var coding = prompt('do you love coding?');
+if(coding.toLowerCase() === 'yes' || coding.toUpperCase() === 'YES'){
+    console.log('You might get good job')
+}
+else {
+    console.log('may be you should find something that you love and be good.');
+}
