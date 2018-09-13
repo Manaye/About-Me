b862c87457a24bf2b75338b1fcf4c293de4d0d45
Manaye <hiwotmanaye2@gmail.com>
2018-09-13 15:01:45 -0700

    added some quations

--- js/app.js
+++ js/app.js
@@ -1,44 +1,81 @@
-    
-  var response = prompt('Are you married?');
-  if(response.toLowerCase() === 'yes'|| response.toUpperCase() === 'YES'){
- console.log('where enjoy your honeymoon?');
-  }
-   else {
-       console.log('then you are a single....!!!');
-   }
+'use strict';
+
+var response = prompt('Are you married?');
+if(response.toLowerCase() === 'yes'|| response.toLowerCase() === 'y'){
+  console.log('where enjoy your honeymoon?');
+  alert('Where you enjoy your honeymoon?');
+}
+else {
+  console.log('then you are a single....!!!');
+}
 
 var ageResponse = prompt('Are you 18 years old or older?');
-if(ageResponse.toLowerCase() === 'yes' || ageResponse.toUpperCase() === 'YES'){
-    console.log('you are eligible to work!!!');
+if(ageResponse.toLowerCase() === 'yes' || ageResponse.toUpperCase() === 'Y'){
+  console.log('you are eligible to work!!!');
 }
 else {
-    console.log('grow up kiddo!!!');
+  console.log('grow up kiddo!!!');
 }
 
-var something = prompt('did you see something in the sky?')
-    if(something.toUpperCase() === 'yes'|| something.toUpperCase() === 'YES'){
-        console.log('i am not imagining!!!');
-    }
+var something = prompt('did you see something in the sky?');
+if(something.toUpperCase() === 'yes'|| something.toUpperCase() === 'YES'){
+  console.log('i am not imagining!!!');
+}
 else {
-    console.log('i guess i am imagining things!!!')
+  console.log('i guess i am imagining things!!!');
 }
 
 
 var movies = prompt('Have you seen Beauty and the Beast?');
 if(movies.toLowerCase() === 'no' || movies.toUpperCase()==='NO' ){
-    console.log('ohh u miss it!!')
+  console.log('ohh u miss it!!');
 }
 else if(movies.toLowerCase() === 'yes' ||movies.toUpperCase()==='YES'){
-    console.log('ohh Me too what a nice movie !!')
+  console.log('ohh Me too what a nice movie !!');
 }
 else {
-    console.log('answer yes or no please!!!');
+  console.log('answer yes or no please!!!');
 }
 
 var coding = prompt('do you love coding?');
 if(coding.toLowerCase() === 'yes' || coding.toUpperCase() === 'YES'){
-    console.log('You might get good job')
+  console.log('You might get good job');
 }
 else {
-    console.log('may be you should find something that you love and be good.');
+  console.log('may be you should find something that you love and be good.');
+}
+
+
+
+var counter = 0;
+var maxTries = 3;
+var FavNumber = 13;
+for(var i = 0; i <= 4; i++){
+  var tries = prompt('what is my fav number? choose between 0 & 20');
+  counter+= 1;
+  if(counter> maxTries){
+    alert('you have reached your maximum tries.');
+    break;
+  }
+  if(tries === FavNumber){
+    alert('you won!!!');
+    break;
+  } 
+}
+
+var brandCounter = 0;
+var maxTriesBrand = 6;
+var favBrand = 'Gucci';
+for(i=0;i<=7;i++){
+    
+  tries= prompt('what is my brand?');
+  brandCounter += 1;
+  if(brandCounter> maxTriesBrand){
+    alert('you have reached your maximum tries.');
+    break;
+  }
+  if(tries === favBrand){
+    alert('you won!!!');
+    break;
+  } 
 }
