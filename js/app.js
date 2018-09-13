941b67ff4a9aec896348574b9bafdd4ea41c8366
Manaye <hiwotmanaye2@gmail.com>
2018-09-13 15:43:01 -0700

    added alert change som style

--- js/app.js
+++ js/app.js
@@ -1,52 +1,61 @@
 'use strict';
 
+var userName = prompt('What is your name?');
+alert('Welcome ' + userName + '!');
+
 var response = prompt('Are you married?');
 if(response.toLowerCase() === 'yes'|| response.toLowerCase() === 'y'){
-  console.log('where enjoy your honeymoon?');
-  alert('Where you enjoy your honeymoon?');
+  console.log('I hope you had a nice honeymoon');
+  alert('I hope you had a nice honeymoon ' + userName);
 }
 else {
   console.log('then you are a single....!!!');
+  alert('then you are a single....!!!');
 }
 
 var ageResponse = prompt('Are you 18 years old or older?');
-if(ageResponse.toLowerCase() === 'yes' || ageResponse.toUpperCase() === 'Y'){
+if(ageResponse.toLowerCase() === 'yes' || ageResponse.toLowerCase() === 'y'){
   console.log('you are eligible to work!!!');
-  alert('Are you 18 years old or older?');
-  
+  alert('you are eligible to work!!!');
 }
 else {
   console.log('grow up kiddo!!!');
+  alert('grow up kiddo!!!');
 }
 
 var something = prompt('did you see something in the sky?');
-if(something.toUpperCase() === 'yes'|| something.toUpperCase() === 'Y'){
+if(something.toUpperCase() === 'yes'|| something.toLowerCase() === 'y'){
   console.log('i am not imagining!!!');
-  alert('did you see something in the sky?');
+  alert('i am not imagining!!!');
 }
 else {
   console.log('i guess i am imagining things!!!');
+  alert('i guess i am imagining things!!!');
 }
 
 
 var movies = prompt('Have you seen Beauty and the Beast?');
-if(movies.toLowerCase() === 'no' || movies.toUpperCase()==='NO' ){
+if(movies.toLowerCase() === 'no' || movies.toLowerCase()==='n' ){
   console.log('ohh u miss it!!');
-  alert('Have you seen Beauty and the Beast?');
+  alert('ohh u miss it!!');
 }
-else if(movies.toLowerCase() === 'yes' ||movies.toUpperCase()==='Y'){
+else if(movies.toLowerCase() === 'yes' ||movies.toLowerCase()==='y'){
   console.log('ohh Me too what a nice movie !!');
+  alert('ohh Me too what a nice movie !!');
 }
 else {
   console.log('answer yes or no please!!!');
+  alert('answer yes or no please!!!');
 }
 
 var coding = prompt('do you love coding?');
-if(coding.toLowerCase() === 'yes' || coding.toUpperCase() === 'Y'){
+if(coding.toLowerCase() === 'yes' || coding.toLowerCase() === 'y'){
   console.log('You might get good job');
+  alert('You might get good job');
 }
 else {
   console.log('may be you should find something that you love and be good.');
+  alert('may be you should find something that you love and be good.');
 }
 
 
